1e6c4a23dec7fe94b33bf3bb14df4ba8a9bf8cf7
Mike McClure <mikemc3579@gmail.com>
2019-02-13 18:16:58 +0000

    Added navbar, Developer Page, Investor Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
     root to:"pages#home"
     get 'about', to: 'pages#about'
+    get 'developers', to: 'pages#developers'
+    get 'investors', to: 'pages#investors'
+
 end
+    
