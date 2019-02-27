d4380c432997c2ac6d26b14eb5931b9a6e98a0ca
Mike McClure <mikemc3579@gmail.com>
2019-02-27 22:55:45 +0000

    Added flash styling to contact messages.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,14 +6,16 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
+            flash[:success] = "Success!"
             redirect_to root_path 
         else
-            redirect_to new_contact_path, notice: "Error Occured"
+            flash[:error] = @contact.errors.full_messages.join(" , ")
+            redirect_to new_contact_path
         end
     end
     
     private
     def contact_params
-        params.require(:contact).permit(:firstname, :lastname, :email, :comments)
+        params.require(:contact).permit(:name, :email, :comments)
     end
 end
