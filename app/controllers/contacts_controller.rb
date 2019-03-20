50f7eb6328c76fec7fba3272b2a260d780902063
Mike McClure <mikemc3579@gmail.com>
2019-03-20 02:35:49 +0000

    Made corrections in contacts_controller file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,9 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contacts_params)
         if @contact.save
-            name = params[:contacts][:name]
-            email = params[:contacts][:email]
-            body = params[:contacts][:comments]
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
             ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Your message has been successfully delivered!"
             redirect_to root_path
